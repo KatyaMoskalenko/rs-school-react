830257b45b7e0d46276318bbed614be77c1ffe15
KatyaMoskalenko <tukai_ekaterina@mail.ru>
2023-03-16 13:14:31 +0200

    feat: add basic routing

--- rs-school-react/src/App.tsx
+++ rs-school-react/src/App.tsx
@@ -1,26 +1,23 @@
+import Header from 'components/Header';
+import AboutUs from 'pages/AboutUs';
+import Error from 'pages/Error';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<AboutUs />}></Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
