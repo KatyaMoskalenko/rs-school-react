44273687f9e894dd7d6de9317fd400a264d5f2d9
KatyaMoskalenko <tukai_ekaterina@mail.ru>
2023-03-20 16:22:55 +0200

    feat: add coverage to package.json

--- rs-school-react/src/App.test.tsx
+++ rs-school-react/src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/About Us/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders App component', () => {
+    render(<App />);
+    expect(screen.getByPlaceholderText(/Search/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Search/i)).not.toBeRequired();
+    expect(screen.getByText(/About Us/i)).toBeInTheDocument();
+  });
 });
