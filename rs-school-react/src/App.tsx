bb3d5f914901c6d5ee580b4241091d21ab1bd8ff
KatyaMoskalenko <tukai_ekaterina@mail.ru>
2023-03-21 15:18:46 +0200

    feat: adjust routing

--- rs-school-react/src/App.tsx
+++ rs-school-react/src/App.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import './App.scss';
-import withRouter from 'components/withRouterHOC/withRouter';
-import Navigation from 'components/navigation/Navigation';
 import AboutUs from 'pages/AboutUs';
 import Error from 'pages/Error';
 import Home from 'pages/home/Home';
-
-const WithRouterHeader = withRouter();
+import Layout from 'components/layout/Layout';
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Navigation />
-        <WithRouterHeader />
+      <>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<AboutUs />}></Route>
-          <Route path="*" element={<Error />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="about" element={<AboutUs />}></Route>
+            <Route path="*" element={<Error />} />
+          </Route>
         </Routes>
-      </BrowserRouter>
+      </>
     );
   }
 }
--- rs-school-react/src/components/navigation/Navigation.tsx
+++ rs-school-react/src/components/navigation/Navigation.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import './Navigation.scss';
 import { NavLink } from 'react-router-dom';
 
+interface setActive {
+  isActive: boolean;
+}
+
+const setActive = ({ isActive }: setActive) =>
+  isActive ? 'active navigation__item' : 'navigation__item';
+
 class Navigation extends React.Component {
   render() {
     return (
       <div className="navigation">
-        <NavLink
-          to={''}
-          className={({ isActive }) => (isActive ? 'active navigation__item' : 'navigation__item')}
-        >
+        <NavLink to={''} className={setActive}>
           Home
         </NavLink>
         <div className="navigation__delimiter">/</div>
-        <NavLink
-          to={'about'}
-          className={({ isActive }) => (isActive ? 'active navigation__item' : 'navigation__item')}
-        >
+        <NavLink to={'about'} className={setActive}>
           About Us
         </NavLink>
       </div>
