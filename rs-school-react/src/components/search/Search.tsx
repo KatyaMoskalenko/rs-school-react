7c8e737eb3ca9ecc36e4dd1b0910f291cd911039
KatyaMoskalenko <tukai_ekaterina@mail.ru>
2023-04-01 21:38:45 +0300

    feat: adjust search component

--- rs-school-react/src/components/search/Search.tsx
+++ rs-school-react/src/components/search/Search.tsx
@@ -1,14 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Search.scss';
 
 export default function Search(): ReturnType<React.FC> {
   const [value, setValue] = useState<string | null>(localStorage.getItem('searchValue'));
+  const searchRef = useRef<string | null>();
+
+  useEffect(() => {
+    searchRef.current = value;
+  }, [value]);
 
   useEffect(() => {
     return () => {
-      localStorage.setItem('searchValue', value ? value : '');
+      localStorage.setItem('searchValue', searchRef.current || '');
     };
-  }, [value]);
+  }, []);
 
   function handleSearch(event: React.ChangeEvent<HTMLInputElement>): void {
     setValue(event.target.value);
